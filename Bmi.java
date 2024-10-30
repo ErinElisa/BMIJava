e60f92cc81a05761a9ca76dc3fe7fa732cbaef72
vvOvvO <108379797+ErinElisa@users.noreply.github.com>
2024-10-31 00:20:18 +0700

    add if in Bmi and comment on both files

--- Bmi.java
+++ Bmi.java
@@ -2,11 +2,18 @@ public class Bmi {
     double bmi;
     
     void getBmi(double height, double weight){
-        //height harus dalam meter
+        //if the height in centimeter
+        if (height > 5) { //assumming height in centimeters is more than 5 meters
+            height = height / 100; 
+        }
+
+        //bmi formula
         bmi = weight / (height * height);
         
+        //to call bmi result
         System.out.printf("BMI ku adalah: %.2f%n", bmi);
 
+        //to determine your bmi cateogry
         if (bmi < 18.5) {
             System.out.println("Kamu Kurang Berat Badan");
         } else if (bmi >= 18.5 && bmi < 24.9) {
--- BmiBeraksi.java
+++ BmiBeraksi.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class BmiBeraksi {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+
+        //create new instance of the Bmi class
         Bmi NewBmi = new Bmi();
 
+        // prompt the user to input their weight in kilograms
         System.out.println("Input berat badan dalam kilogram: ");
         double weight = sc.nextDouble();
 
+        // prompt the user to input their height in meters
         System.out.println("Input tinggi badan dalam meter: ");
         double height = sc.nextDouble();
 
+        // call getBmi to calculate and display BMI with the given height and weight
         NewBmi.getBmi(height, weight);
         
         sc.close();
